574069b9095eb8e5ed7f74dda03d0703d14e6a47
pierreviences <imaniyazid@gmail.com>
2023-08-16 21:13:26 +0700

    add: Add library auth firebase

---
+++ app/src/main/java/com/example/latihanfirebase/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.latihanfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
