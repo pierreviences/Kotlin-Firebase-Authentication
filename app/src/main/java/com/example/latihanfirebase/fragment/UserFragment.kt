5b3fa422238bee7c2b1fd933f82218542c70177d
pierreviences <imaniyazid@gmail.com>
2023-08-17 09:24:31 +0700

    feat: Add Firebase Storage Image Upload Functionality

--- app/src/main/java/com/example/latihanfirebase/fragment/UserFragment.kt
+++ app/src/main/java/com/example/latihanfirebase/fragment/UserFragment.kt
@@ -1,7 +1,11 @@
 package com.example.latihanfirebase.fragment
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,11 +15,14 @@ import com.example.latihanfirebase.LoginActivity
 import com.example.latihanfirebase.R
 import com.example.latihanfirebase.databinding.FragmentUserBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class UserFragment : Fragment() {
 
     private var _binding : FragmentUserBinding? = null
     lateinit var auth: FirebaseAuth
+    private lateinit var imgUri : Uri
 
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -44,6 +51,10 @@ class UserFragment : Fragment() {
             binding.edtEmail.setText(user.email)
         }
 
+        binding.cviUser.setOnClickListener {
+            goToCamera()
+        }
+
         binding.btnLogout.setOnClickListener {
             btnLogout()
         }
@@ -56,6 +67,51 @@ class UserFragment : Fragment() {
         val intent = Intent(context, LoginActivity::class.java)
         startActivity(intent)
         activity?.finish()
+    }
+
+
+    private fun goToCamera(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
+            intent ->
+            activity?.packageManager?.let{
+                intent?.resolveActivity(it).also{
+                    startActivityForResult(intent, REQ_CAM)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQ_CAM && resultCode == RESULT_OK){
+            val imgBitMap = data?.extras?.get("data") as Bitmap
+            uploadImgToFirebase(imgBitMap)
+        }
+    }
+
+    private fun uploadImgToFirebase(imgBitmap: Bitmap){
+        val baos = ByteArrayOutputStream()
+        val ref = FirebaseStorage.getInstance().reference.child("img_user/${FirebaseAuth.getInstance().currentUser?.email}")
+        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+
+        val img = baos.toByteArray()
+        ref.putBytes(img)
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    ref.downloadUrl.addOnCompleteListener{
+                        Task ->
+                        Task.result.let{
+                            Uri ->
+                            imgUri = Uri
+                            binding.cviUser.setImageBitmap(imgBitmap)
+                        }
+                    }
+                }
+            }
+    }
+
+    companion object{
+        const val REQ_CAM = 100
     }
 
 }
