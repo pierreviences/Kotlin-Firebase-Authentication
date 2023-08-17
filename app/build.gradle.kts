f2cf63cc8da348f369489e9e2ab312aae865be70
pierreviences <imaniyazid@gmail.com>
2023-08-17 09:09:57 +0700

    feat: Add plugin firebase storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,12 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
 }
