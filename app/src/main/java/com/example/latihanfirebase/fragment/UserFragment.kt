bffaf9706a65d7320b8e5a26fbba91bc925a0685
pierreviences <imaniyazid@gmail.com>
2023-08-17 08:45:41 +0700

    feat: Tampilan profil pengguna dan logout

--- app/src/main/java/com/example/latihanfirebase/fragment/UserFragment.kt
+++ app/src/main/java/com/example/latihanfirebase/fragment/UserFragment.kt
@@ -1,10 +1,13 @@
 package com.example.latihanfirebase.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.latihanfirebase.LoginActivity
 import com.example.latihanfirebase.R
 import com.example.latihanfirebase.databinding.FragmentUserBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -24,7 +27,35 @@ class UserFragment : Fragment() {
         return binding.root
     }
 
-    
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        if(user != null) {
+            binding.edtEmail.setText(user.displayName)
+            binding.edtEmail.setText(user.email)
+        }
+
+        binding.btnLogout.setOnClickListener {
+            btnLogout()
+        }
+
+    }
+
+    private fun btnLogout(){
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        val intent = Intent(context, LoginActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
+    }
 
 }
