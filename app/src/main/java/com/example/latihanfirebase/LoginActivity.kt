f03305654e58727491a10079a1ee0c743c7ba16e
pierreviences <imaniyazid@gmail.com>
2023-08-17 08:18:51 +0700

    feat: Add Function LoginFirebase

--- app/src/main/java/com/example/latihanfirebase/LoginActivity.kt
+++ app/src/main/java/com/example/latihanfirebase/LoginActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
+import android.widget.Toast
 import com.example.latihanfirebase.databinding.ActivityLoginBinding
 import com.example.latihanfirebase.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-
+    lateinit var auth: FirebaseAuth
     lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -44,13 +46,22 @@ class LoginActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            // validasi panjang password
-            if(password.length < 6){
-                binding.edtPasswordLogin.error = "Password minimal 6 karakter"
-                binding.edtPasswordLogin.requestFocus()
-                return@setOnClickListener
-            }
+
+            LoginFirebase(email,password)
 
         }
+    }
+
+    private fun LoginFirebase(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if(it.isSuccessful){
+                    Toast.makeText(this, "Selamat Datang $email", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }else {
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
